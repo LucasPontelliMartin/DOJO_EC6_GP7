1983a14e341f19da4d37ab8110ad50f2abebffc1
Lucas Pontelli Martin <pontelli.lucas@gmail.com>
2021-03-07 22:37:11 -0300

    Primeiro Commit

--- src/dojo_analisadorlexico/Dojo_AnalisadorLexico.java
+++ src/dojo_analisadorlexico/Dojo_AnalisadorLexico.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -17,37 +19,65 @@ import java.util.Scanner;
  */
 public class Dojo_AnalisadorLexico {
 
-      public static void main(String[] args) throws FileNotFoundException {
+      public static void main(String[] args) throws FileNotFoundException, IOException {
         
     String conteudo = readFile();
-    
-     try {
-            conteudo = conteudo.replaceAll("(?s:/\\*.*?\\*/)|//.*", "") ;
+    ArrayList<String> palavras = readFileWords();
+    conteudo = conteudo.replaceAll("([ \t]*\\/\\/.*)", "");
+    conteudo = conteudo.replaceAll("\\/\\*([\\S\\s]+?)\\*\\/", "");
+    String lines[] = conteudo.split("\\r?\\n");
+    System.out.println(conteudo);
+
             FileWriter fw = new FileWriter("Formatado.txt");
             fw.write(conteudo);
             fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    
-    System.out.println(conteudo);
-    }
-    
-
-
+}
 private static String readFile() throws FileNotFoundException{
 
 String conteudo = null;
-
+String linha;
 File texto = new File ("CodigoFonte_JS.txt");
 Scanner myReader = new Scanner(texto);
-        
-
         while (myReader.hasNext()){
-            conteudo += myReader.nextLine();
+            linha = myReader.nextLine();
+            
+            if (!linha.contains("//")) {
+                conteudo += linha;
+            }
         }
         return conteudo;
 }
 
-    
+private static ArrayList<String> readFileWords() throws FileNotFoundException{
+
+String conteudo = null;
+String linha;
+ArrayList<String> palavras = new ArrayList<String> ();
+File texto = new File ("Words.txt");
+Scanner myReader = new Scanner(texto);
+        while (myReader.hasNext()){
+            linha = myReader.nextLine();
+            
+            if (!linha.contains("//")) {
+                palavras.add(linha);
+            }
+        }
+        return palavras;
 }
+
+private static void Analisa(String conteudo,ArrayList<String> palavras ) throws FileNotFoundException{
+
+String linha;
+File texto = new File ("CodigoFonte_JS.txt");
+Scanner myReader = new Scanner(texto);
+        while (myReader.hasNext()){
+            linha = myReader.nextLine();
+            
+            if (!linha.contains("//")) {
+                conteudo += linha;
+            }
+        }
+}
+
+    }
+        
